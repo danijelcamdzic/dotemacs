66f4f678f32f73b278bb0c58c695850ecad9396c
danijelcamdzic <danijelcamdzic@tuta.com>
2023-12-04 16:45:41 +0100

    Change date view on dashboard agenda

--- elisp/editor-config.el
+++ elisp/editor-config.el
@@ -71,11 +71,46 @@
 (setq dashboard-set-init-info t)
 (setq dashboard-set-heading-icons t)
 (setq dashboard-agenda-time-string-format "%Y-%m-%d %H:%M")
+(add-to-list 'dashboard-items '(agenda) t)
+
+(defun my/dashboard-agenda--formatted-time-advice (orig-fun &rest args)
+  "Modifies the display of time in the dashboard agenda.
+If the time corresponds to 'today', 'yesterday', or 'tomorrow', it replaces the date with these words.
+Keeps the time part unless it's exactly 00:00, in which case only the relative date is displayed."
+  (let* ((original-time-string (apply orig-fun args))
+         (entry-time (org-get-scheduled-time (point)))
+         (relative-date (my/relative-date entry-time))
+         (time-part (format-time-string "%H:%M" entry-time))
+         (time-is-midnight (string= time-part "00:00")))
+    (if relative-date
+        (if time-is-midnight
+            relative-date
+          (concat relative-date " " time-part))
+      original-time-string)))
+
+(defun my/relative-date (time)
+  "Determines if the given TIME is 'today', 'yesterday', or 'tomorrow'.
+Returns the corresponding string or nil if the time doesn't match any of these.
+TIME is expected to be in Emacs internal time format."
+  (when time
+    (let* ((current-time (current-time))
+           (current-date (decode-time current-time))
+           (entry-date (decode-time time))
+           (current-day-num (time-to-days (apply #'encode-time (decode-time (current-time)))))
+           (entry-day-num (time-to-days (apply #'encode-time (decode-time time))))
+           (day-difference (- current-day-num entry-day-num)))
+      (cond ((eq day-difference 0) "today")
+            ((eq day-difference 1) "yesterday")
+            ((eq day-difference -1) "tomorrow")))))
+
+;; Add advice to change the date format to 'yesterday', 'today' or 'tomorrow' if it suits
+(advice-add 'dashboard-agenda--formatted-time :around #'my/dashboard-agenda--formatted-time-advice)
+
+;; Set up sorting strategy
 (setq dashboard-agenda-sort-strategy  '((agenda time-up priority-down category-keep)
                                         (todo time-up priority-down category-keep)
                                         (tags time-up priority-down category-keep)
                                         (search time-up priority-down category-keep)))
-(add-to-list 'dashboard-items '(agenda) t)
 (dashboard-setup-startup-hook)
 
 ;; Provide package for use
