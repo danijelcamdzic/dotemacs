4b15b7ca7e423c392ebfd0f2f7c8b3462321c7f8
Danijel Camdzic <danijelcamdzic@tuta.com>
2023-12-27 16:00:31 +0100

    Add C/C++ babel support

--- elisp/org-config.el
+++ elisp/org-config.el
@@ -32,8 +32,7 @@
     (org-babel-do-load-languages
      'org-babel-load-languages
      '((python . t)    ; Enable Python
-       (C . t)         ; Enable C
-       (cpp . t)))     ; Enable C++
+       (C . t)))       ; Enable C
 
     ;; Python configuration
     (setq org-babel-python-command "python3")
