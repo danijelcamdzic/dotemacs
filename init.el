f1df70fa673ac99c0c1c60584b8d03fe6c706a24
Danijel Camdzic <danijelcamdzic@tuta.com>
2024-01-16 15:15:04 +0100

    Remove dashboard package

--- init.el
+++ init.el
@@ -234,47 +234,6 @@
 (when (eq system-type 'gnu/linux)
   (dc/gui-hide-bars))
 
-;;; Dashboard
-;;;; Configuration
-(use-package dashboard
-  :ensure t
-  :config
-  ;; Set the startup appearance. Make it minimal and remove almost everything
-  (setq dashboard-startup-banner 'official
-        dashboard-center-content t
-        dashboard-banner-logo-title "Welcome to Emacs"
-        dashboard-items nil
-        dashboard-set-footer nil
-        dashboard-set-init-info t
-        dashboard-set-heading-icons t
-        dashboard-agenda-prefix-format "%s %t"
-        dashboard-agenda-time-string-format "%Y-%m-%d %H:%M"
-        dashboard-agenda-sort-strategy '(time-up))
-  (add-to-list 'dashboard-items '(agenda) t)
-
-  ;; Set up startup hook
-  (dashboard-setup-startup-hook)
-  )
-
-;;;; Functions - Agenda Dashboard Relative Days
-(defun dc/dashboard-agenda--formatted-time--use-relative-days-advice (orig-fun &rest args)
-  "Modifies the display of time in the dashboard agenda.
-If the time corresponds to 'today', 'yesterday', or 'tomorrow', it replaces the date with these words.
-Keeps the time part unless it's exactly 00:00, in which case only the relative date is displayed."
-  (let* ((original-time-string (apply orig-fun args))
-         (entry-time (org-get-scheduled-time (point)))
-         (relative-date (dc/time-relative-date entry-time))
-         (time-part (format-time-string "%H:%M" entry-time))
-         (time-is-midnight (string= time-part "00:00")))
-    (if relative-date
-        (if time-is-midnight
-            relative-date
-          (concat relative-date " " time-part))
-      original-time-string)))
-
-;; Add advice to change the date format of agenda items to 'yesterday', 'today' or 'tomorrow'
-(advice-add 'dashboard-agenda--formatted-time :around #'dc/dashboard-agenda--formatted-time--use-relative-days-advice)
-
 ;;; Org-mode
 ;;;; Org
 ;;;;; Configuration
