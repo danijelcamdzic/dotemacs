16475014006b268ff609adb727d9f4c2cbae0c96
Danijel Camdzic <danijelcamdzic@tuta.com>
2024-06-16 12:12:20 +0200

    Remove auth-info TOTP and password options

--- init.el
+++ init.el
@@ -10,8 +10,8 @@
 
 (require 'package)
 
-;; Temporarily disable signature checks
-(setq package-check-signature nil)
+;; Enable package signature checks
+(setq package-check-signature t)
 
 ;; Initialize packages
 (package-initialize)
@@ -22,7 +22,7 @@
 
 ;; Add melpa package archives
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 
 ;;;; Package - use-package
 
@@ -53,7 +53,7 @@
 
 (use-package quelpa-use-package
   :ensure t
-  :after (quelpa)
+  :after quelpa
   :config
   ;; Activate quelpa-use-package
   (quelpa-use-package-activate-advice)
@@ -1925,189 +1925,6 @@ am start -a android.intent.action.VIEW -t video/* -d file:///storage/emulated/0/
   ;; environment where GUI pinentry dialogs are not available
   (setq epa-pinentry-mode 'loopback)
   )
-
-;;;; Package - auth-source
-
-;;;;; Configuration
-
-(use-package auth-source
-  :ensure t
-  :config
-  ;; Set auth-sources files
-  (setq auth-sources
-        (cl-loop for file in (directory-files (concat dc-documents-directory ".auth-sources/") t "\\.gpg$")
-                 collect `(:source ,file)))
-
-  ;; Enable authinfo-mode for auth-source files
-  (add-to-list 'auto-mode-alist '("\\.authinfo.*\\.gpg\\'" . authinfo-mode))
-
-  ;; Clear cached passwords after buffers are switched
-  (add-hook 'buffer-list-update-hook 'auth-source-forget-all-cached)
-  )
-
-;;;;; Function - Get auth-source secrets
-
-(defun dc/auth-display-all (search-string)
-  "Select an authentication entry based on SEARCH-STRING from `auth-sources', and briefly display its value.
-Function decrypts all auth-source files and lets you search through them to find the SEARCH-STRING."
-  (interactive "sEnter authentication type: ")
-  (let* ((candidates (mapcar
-                      (lambda (auth)
-                        (cons (format "User '%s' on %s"
-                                      (propertize (plist-get auth :user) 'face 'font-lock-keyword-face)
-                                      (propertize (plist-get auth :host) 'face 'font-lock-string-face))
-                              auth))
-                      (seq-filter (lambda (auth) (string-prefix-p search-string (plist-get auth :host)))
-                                  (auth-source-search :max 10000))))
-         (selected-auth (cdr (assoc (completing-read (format "Pick an entry for '%s'> " search-string) candidates) candidates)))
-         (password (funcall (plist-get selected-auth :secret))))
-    ;; Temporarily disable logging in *Messages* buffer
-    (let ((message-log-max nil))
-      (message "Your authentication secret for '%s' is: %s"
-               (propertize (plist-get selected-auth :host) 'face 'font-lock-keyword-face)
-               (propertize password 'face 'font-lock-string-face)))))
-
-(defun dc/auth-search (search-string)
-  "Select a .gpg file from `auth-sources`, then select an authentication entry from that file, and briefly display its value.
-Function decrypts only the chosen auth-source file and lets you search through it to find the SEARCH-STRING."
-  (interactive "sEnter authentication type: ")
-  (let* ((gpg-files (mapcar (lambda (source) (plist-get source :source)) auth-sources))
-         (selected-file (completing-read "Select an auth-source file: " gpg-files))
-         (candidates (mapcar
-                      (lambda (auth)
-                        (cons (format "User '%s' on %s"
-                                      (propertize (plist-get auth :user) 'face 'font-lock-keyword-face)
-                                      (propertize (plist-get auth :host) 'face 'font-lock-string-face))
-                              auth))
-                      (seq-filter (lambda (auth) (string-prefix-p search-string (plist-get auth :host)))
-                                  (auth-source-search :max 10000 :source selected-file)))))
-    (let* ((selected-entry (completing-read "Pick an entry> " candidates))
-           (auth (cdr (assoc selected-entry candidates)))
-           (password (funcall (plist-get auth :secret))))
-      ;; Temporarily disable logging in *Messages* buffer
-      (let ((message-log-max nil))
-        (message "Your authentication secret for '%s' is: %s"
-                 (propertize (plist-get auth :host) 'face 'font-lock-keyword-face)
-                 (propertize password 'face 'font-lock-string-face))))))
-
-;;;;; Function - Get passwords
-
-(defun dc/password-display-all ()
-  "Uses `dc/auth-get-secret-all' function to retrieve the passwords from all files in `auth-sources' by searching for PASS string."
-  (interactive)
-  (dc/auth-display-all "PASS:"))
-
-(defun dc/password-search ()
-  "Uses `dc/auth-get-secret-one' function to retrieve the passwords from the chosen `.gpg' files in `auth-sources'
-by searching for PASS string."
-  (interactive)
-  (dc/auth-search "PASS:"))
-
-;;;;; Function - Get TOTP
-
-(require 'bindat)
-(require 'gnutls)
-(require 'hexl)
-
-(defun totp--base32-char-to-n (char)
-  "Return 5 bit integer value matching base32 CHAR."
-  (cond ((<= ?A char ?Z) (- char ?A))
-        ((<= ?a char ?z) (- char ?a))
-        ((<= ?2 char ?7) (+ (- char ?2) 26))
-        (t (error "Invalid number range"))))
-
-(defun totp--base32-to-number (string)
-  "Base32-decode STRING and return the result as number.
-Handles interleaved whitespaces and missing padding charachters
-gracefuly (The number of padding chars can be deduced from input
-length)."
-  (let* ((s (replace-regexp-in-string "\\([[:space:]]\\|=*$\\)" "" string))
-         (ntrail (mod (* 5  (length s)) 8)))
-    (ash (seq-reduce (lambda (acc char)
-                       (+ (ash acc 5) (totp--base32-char-to-n char)))
-                     s 0) (- ntrail))))
-
-(defun totp--hex-decode-string (string)
-  "Hex-decode STRING and return the result as a unibyte string."
-  (apply #'unibyte-string
-         (seq-map (lambda (s) (hexl-htoi (aref s 0) (aref s 1)))
-                  (seq-partition string 2))))
-
-(defun totp (string &optional time digits)
-  "Return a TOTP token using the secret STRING and current time.
-TIME is used as counter value instead of current time, if non-nil.
-DIGITS is tre  number of pin digits and defaults to 6."
-  (let* ((hex-string (if (string-match-p "^[0-9a-fA-F]\\{2\\}+$" string)
-                         string		;already in hex format
-                       (format "%X" (totp--base32-to-number string))))
-         (key-bytes (totp--hex-decode-string (upcase hex-string)))
-         (counter (truncate (/ (or time (time-to-seconds)) 30)))
-         (digits (or digits 6))
-         (format-string (format "%%0%dd" digits))
-         ;; we have to manually split the 64 bit number (u64 not supported in Emacs 27.2)
-         (counter-bytes (bindat-pack  '((:high u32) (:low u32))
-                                      `((:high . ,(ash counter -32)) (:low . ,(logand counter #xffffffff)))))
-         (mac (gnutls-hash-mac 'SHA1 key-bytes counter-bytes))
-         (offset (logand (bindat-get-field (bindat-unpack '((:offset u8)) mac 19) :offset) #xf)))
-    (format format-string
-            (mod
-             (logand (bindat-get-field (bindat-unpack '((:totp-pin u32)) mac  offset) :totp-pin)
-                     #x7fffffff)
-             (expt 10 digits)))))
-
-(defun dc/totp-display-all (auth)
-  "Select a TOTP AUTH from `auth-sources', display its TOTP, and show remaining valid time.
-Function decrypts all auth-source files and lets you search through them to find the TOTP."
-  (interactive
-   (list
-    (let ((candidates (mapcar
-                       (lambda (auth)
-                         (cons (format "User '%s' on %s"
-                                       (propertize (plist-get auth :user) 'face 'font-lock-keyword-face)
-                                       (propertize (plist-get auth :host) 'face 'font-lock-string-face))
-                               auth))
-                       (seq-filter (lambda (auth) (string-prefix-p "TOTP:" (plist-get auth :host)))
-                                   (auth-source-search :max 10000)))))
-      (cdr (assoc (completing-read "Pick a TOTP> " candidates) candidates)))))
-  (let* ((current-time (time-to-seconds))
-         (time-step 30)
-         (time-remaining (- time-step (mod current-time time-step)))
-         (code (totp (funcall (plist-get auth :secret)))))
-    ;; Temporarily disable logging in *Messages* buffer
-    (let ((message-log-max nil))
-      (message "Your TOTP for '%s' is: %s (valid for %d more seconds)"
-               (propertize (plist-get auth :host) 'face 'font-lock-keyword-face)
-               (propertize code 'face 'font-lock-string-face)
-               time-remaining))
-    code))
-
-(defun dc/totp-search ()
-  "Select a TOTP AUTH from `auth-sources', display its TOTP, and show remaining valid time.
-Function decrypts only the chosen auth-source file and lets you search through it to find the TOTP."
-  (interactive)
-  (let* ((gpg-files (mapcar (lambda (source) (plist-get source :source)) auth-sources))
-         (selected-file (completing-read "Select an auth-source file: " gpg-files))
-         (candidates (mapcar
-                      (lambda (auth)
-                        (cons (format "User '%s' on %s"
-                                      (propertize (plist-get auth :user) 'face 'font-lock-keyword-face)
-                                      (propertize (plist-get auth :host) 'face 'font-lock-string-face))
-                              auth))
-                      (seq-filter (lambda (auth) (string-prefix-p "TOTP:" (plist-get auth :host)))
-                                  (auth-source-search :max 10000 :source selected-file)))))
-    (let* ((selected-entry (completing-read "Pick a TOTP> " candidates))
-           (auth (cdr (assoc selected-entry candidates)))
-           (current-time (time-to-seconds))
-           (time-step 30)
-           (time-remaining (- time-step (mod current-time time-step)))
-           (code (totp (funcall (plist-get auth :secret)))))
-      ;; Temporarily disable logging in *Messages* buffer
-      (let ((message-log-max nil))
-        (message "Your TOTP for '%s' is: %s (valid for %d more seconds)"
-                 (propertize (plist-get auth :host) 'face 'font-lock-keyword-face)
-                 (propertize code 'face 'font-lock-string-face)
-                 time-remaining))
-      code)))
 
 ;;; Finances
 
